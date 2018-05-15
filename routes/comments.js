e974ce893eb39c3b8d343f346d55be5e45bb7c65
Damián Adams <contact@mianlabs.com>
2018-05-14 23:54:59 -0400

    Add the campground comments delete route and form

--- routes/comments.js
+++ routes/comments.js
@@ -65,6 +65,18 @@ router.put('/:comment_id', (req, res) => {
 	});
 });
 
+// DESTROY ROUTE
+
+router.delete('/:comment_id', (req, res) => {
+	Comment.findByIdAndRemove(req.params.comment_id, function(err) {
+		if (err) {
+			res.redirect('back');
+		} else {
+			res.redirect('/campgrounds/' + req.params.id);
+		}
+	});
+});
+
 
 function isLoggedIn(req, res, next) {
 	if (req.isAuthenticated()) {
